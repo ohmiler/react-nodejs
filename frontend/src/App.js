e499839613fcb09b6d732b5017888eb122a8a43b
Patiphan Phengpao <misterpatipan123@gmail.com>
2021-09-23 12:15:20 +0700

    add front end files

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Quotes from './Quotes'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Quotes />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Quotes.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+function Quotes() {
+
+    const [text, setText] = useState("");
+    const [author, setAuthor] = useState("");
+
+    function getQuote() {
+        axios.get('http://localhost:5000/', { crossdomain: true })
+             .then(response => {
+                 setText(response.data.text);
+                 setAuthor(response.data.author);
+             })
+    }
+
+    return (
+        <div>
+            <button onClick={getQuote}>
+                Generate Quote
+            </button>
+            <h1>{text}</h1>
+            <h3>{"-" + author}</h3>
+        </div>
+    )
+}
+
+export default Quotes
